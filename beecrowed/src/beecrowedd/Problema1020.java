9cd0adf5aa753cf0c4585caf9398d4e96018cf92
Clayton Salvador <43305715+claytonrh@users.noreply.github.com>
2023-01-27 11:59:53 -0300

    Create Problema1020.java

---
+++ beecrowed/src/beecrowedd/Problema1020.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main (String[]args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int valor = input.nextInt();
+		int a = 365;
+		int b = 30;
+		
+		int ano = valor / a;
+		valor = valor % a;	
+		
+		int mes = valor / b;
+		valor = valor % b;
+		    
+		int dia = valor % b;
+		
+		System.out.printf(ano + " ano(s) \n");		
+		System.out.printf(mes + " mes(es) \n");		
+		System.out.printf(dia + " dia(s) \n");
+		
+		
+		input.close();
+	}
+}
