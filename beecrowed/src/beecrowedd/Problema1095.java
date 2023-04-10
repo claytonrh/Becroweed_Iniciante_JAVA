b34bfb71e5517cdb3cfafb8aaa21a89bcab0ac68
Clayton Salvador <43305715+claytonrh@users.noreply.github.com>
2023-04-10 17:02:29 -0300

    Create Problema1095.java

---
+++ beecrowed/src/beecrowedd/Problema1095.java
@@ -0,0 +1,16 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		
+	int j = 60;
+	int i = 1;	
+	
+	System.out.println("I=" + i + " " + "J=" + j);
+	
+	for(int n = 0; j >= 0; n++) {
+		j = j -5;
+		i = i + 3;
+		System.out.println("I=" + i + " " + "J=" + j);		
+		}		
+	}
+}
