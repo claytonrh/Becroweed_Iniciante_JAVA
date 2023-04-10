f59cae8102366e945abb4ef0eb178bcf620d594c
Clayton Salvador <43305715+claytonrh@users.noreply.github.com>
2023-04-10 17:02:56 -0300

    Create Problema1096.java

---
+++ beecrowed/src/beecrowedd/Problema1096.java
@@ -0,0 +1,19 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		
+	int j = 7;
+	int i = 1;	
+	
+	System.out.println("I=" + i + " " + "J=7");		
+	System.out.println("I=" + i + " " + "J=6");		
+	System.out.println("I=" + i + " " + "J=5");		
+	
+	for(int n = 0; i < 9; n++) {
+		i = i + 2;
+		System.out.println("I=" + i + " " + "J=7");		
+		System.out.println("I=" + i + " " + "J=6");		
+		System.out.println("I=" + i + " " + "J=5");		
+		}		
+	}
+}
