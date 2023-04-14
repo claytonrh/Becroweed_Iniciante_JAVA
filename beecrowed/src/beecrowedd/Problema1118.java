e65a69db3b54ef80843f2fe4eeb438caeabdb7be
Clayton Salvador <43305715+claytonrh@users.noreply.github.com>
2023-04-14 17:19:47 -0300

    Create Problema1118.java

---
+++ beecrowed/src/beecrowedd/Problema1118.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main (String[] args) {
+	    
+	    Scanner input = new Scanner(System.in);
+		
+		double nota1;
+		double nota2;	
+		int novoCalculo;	
+		
+		do{
+		
+		do {
+			nota1 = input.nextDouble();
+			if( nota1 < 0 || nota1 > 10) {
+				System.out.println("nota invalida");
+			    }
+			}while(nota1 < 0 || nota1 > 10);
+		
+		do {
+			nota2 = input.nextDouble();
+			if(nota2 < 0 || nota2 > 10) {
+				System.out.println("nota invalida");
+				}
+			}while ( nota2 < 0 || nota2 > 10);
+		
+		double media = (nota1 + nota2) / 2;
+		
+		System.out.printf("media = %.2f\n",media);						
+		
+		do{		
+			System.out.println("novo calculo (1-sim 2-nao)");					
+			novoCalculo = input.nextInt();									
+		}while(novoCalculo < 1 || novoCalculo > 2); 
+		
+		}while(novoCalculo != 2);		
+		
+		input.close();
+	}
+}
