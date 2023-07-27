6ac032b5243702d1a08b95eccfae875a90263ca5
Clayton Salvador <43305715+claytonrh@users.noreply.github.com>
2023-07-27 10:51:32 -0300

    Create Problema1144.java

---
+++ beecrowed/src/beecrowedd/Problema1144.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int linhas = input.nextInt();
+		
+		int valor = 0;
+		for(int i = 1; valor < linhas; i++) {
+			valor += 1;
+			double valor2 = Math.pow(i, 2);
+			double valor3 = Math.pow(i, 3);
+			System.out.println(i + " " + (int)valor2 + " " + (int)valor3);			
+			System.out.println(i + " " + ((int)valor2 + 1) + " " + ((int)valor3 + 1));	
+		}
+		
+		input.close();
+	}
+}
